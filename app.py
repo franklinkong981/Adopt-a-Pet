4148b56e06581c7e601faf6e50a02eb28ff183f5
Franklin Kong <franklinkong981@gmail.com>
2024-04-10 15:20:45 -0700

    Added functionality to add new pet to database upon form submission

--- app.py
+++ app.py
@@ -23,8 +23,19 @@ def list_pets():
 
 @app.route('/add', methods=['GET', 'POST'])
 def show_add_pet_form():
+    """If it's not a POST request being sent to this route OR the form isn't validated, shows the add new pet form. Otherwise, processes the form,
+    adds the new pet to the adopt_a_pet database, and redirects to the home page."""
     form = AddPetForm()
     if form.validate_on_submit():
+        name = form.name.data
+        species = form.species.data
+        photo_url = form.photo_url.data
+        age = form.age.data
+        notes = form.notes.data
+
+        new_pet = Pet(name=name,species=species, photo_url=photo_url, age=age, notes=notes)
+        db.session.add(new_pet)
+        db.session.commit()
         return redirect('/')
     else:
         return render_template('add_pet_form.html', form=form)
